6b449bb4823d5c7b341c9e080e29172014f516d6
charley43613 <41710701+charley43613@users.noreply.github.com>
2020-02-23 23:36:45 -0500

    minor changes to login, created managerServlet v1 with forwarding working

---
+++ src/main/java/com/revature/servlets/ManagerServlet.java
@@ -0,0 +1,47 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+import com.revature.dao.UserImp;
+import com.revature.model.User;
+
+public class ManagerServlet extends HttpServlet {
+
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManagerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		String userId = session.getAttribute("logged User").toString();
+		response.getWriter().append("Forwarded from login page logged User is "+ userId ).append(request.getContextPath());
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+}
+}
