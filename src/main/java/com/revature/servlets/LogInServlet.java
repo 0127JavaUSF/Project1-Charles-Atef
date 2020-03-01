5cd0e3e2395ea5a2dcc6e4ed19f2679944013b93
charley43613 <41710701+charley43613@users.noreply.github.com>
2020-02-29 23:25:26 -0500

    added header allowance for json in servlet, modified post login to accept json, must add for other servlets too TODO

--- src/main/java/com/revature/servlets/LogInServlet.java
+++ src/main/java/com/revature/servlets/LogInServlet.java
@@ -3,7 +3,6 @@ package com.revature.servlets;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +12,7 @@ import javax.servlet.http.HttpSession;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.dao.UserImp;
 import com.revature.model.User;
+import com.revature.model.UserLogin;
 
 /**
  * Servlet implementation class LogInServlet
@@ -28,8 +28,10 @@ public class LogInServlet extends HttpServlet {
     @Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.addHeader("Access-Control-Allow-Headers", "authorization");
+		resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
 		resp.addHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE");
 		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		
 		
 		// TODO Auto-generated method stub
 		super.service(req, resp);
@@ -67,13 +69,18 @@ public class LogInServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String name = request.getParameter("username");
-		String password = request.getParameter("password");
+		ObjectMapper om = new ObjectMapper();
+		UserLogin usrLogin = new UserLogin();
+		usrLogin = om.readValue(request.getReader(), UserLogin.class);
+		
+		
+//		String name = request.getParameter("username");
+//		String password = request.getParameter("password");
 		UserImp userImp = new UserImp();
 		User user = null;
 		HttpSession session = request.getSession();//changed 2/23 11:03 charles
 		try {
-			 user = userImp.logIn(name, password);
+			 user = userImp.logIn(usrLogin.getUsername(), usrLogin.getPassword());
 			 System.out.println("GET THE USER MAYBE, HERES TOSTRING METHOD: " + user.toString());
 			//session.setAttribute("logged User",user.getUserID());//sets session attribute to integer
 			//session.setAttribute("user role", user.getUserRoleId());//also integer
